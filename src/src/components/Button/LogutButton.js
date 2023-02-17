af600380abe39a82e13c02a49528c89b23165827
kidmun <kidusguade41@gmail.com>
2023-02-17 00:40:01 -0800

    second commit

--- src/src/components/Button/LogutButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import './LogoutButton.css';
-const LogoutButton = (props) => {
-    return <button id="button-logout" type="button" onClick={props.onClick}>Logout</button>
-};
-
-export default LogoutButton;
--- src/src/util/url.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = "https://connect-kidus.herokuapp.com";
